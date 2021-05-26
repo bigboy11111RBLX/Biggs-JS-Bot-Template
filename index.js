5b7ece83f7da95bb452686fe174f98e5846328a7
Bigboy_11111 <79596269+bigboy11111RBLX@users.noreply.github.com>
2021-05-26 19:09:18 -0400

    index.js updated

--- index.js
+++ index.js
@@ -9,19 +9,23 @@ client.commands = new Discord.Collection();
 
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+//---command---
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	// set a new item in the Collection
 	// with the key as the command name and the value as the exported module
 	client.commands.set(command.name, command);
 }
-
-
-
-client.once('ready', () => {
-	console.log('Ready!'); //outputs Ready! once bot is online and working
-});
+//---event---
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
 
 
 
