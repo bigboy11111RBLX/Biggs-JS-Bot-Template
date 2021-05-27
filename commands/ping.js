94140605e95986e5718112f24e4b3b0111e198eb
Bigboy_11111 <79596269+bigboy11111RBLX@users.noreply.github.com>
2021-05-27 18:13:26 -0400

    Added response time

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,7 @@ module.exports = {
 	name: 'ping', //name
 	description: 'Ping!', //description
 	execute(message, args) { //uses message and args from index.js
-		message.channel.send('Pong!'); //sends Pong!
+		message.channel.send('Pong!') //sends pong!
+		message.channel.send(new Date().getTime() - message.createdTimestamp + " ms"); //sends bot delay!
 	},
 };
