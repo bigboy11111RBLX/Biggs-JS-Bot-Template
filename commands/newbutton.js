6a3369840e9f3b9e71985342211eb7dad03fcedd
Bigboy_11111 <79596269+bigboy11111RBLX@users.noreply.github.com>
2021-06-08 14:10:11 -0400

    //s

--- commands/newbutton.js
+++ commands/newbutton.js
@@ -1,15 +1,15 @@
 const { MessageButton } = require('discord-buttons');
 module.exports = {
-	name: 'button', //name
+	name: 'button1', //name
 	description: 'Ping!', //description
 	execute(message, args, client) { //uses message and args from index.js
         let button = new MessageButton()
-        .setStyle('url')
-        .setURL('https://npmjs.com/discord-buttons') 
-        .setLabel('My First URL Button!') 
-        .setDisabled(); 
+        .setStyle('url') // url type out of URL, GREEN, RED, and more
+        .setURL('https://npmjs.com/discord-buttons') // url if u choose url
+        .setLabel('My First URL Button!')  // name on button
+        .setDisabled(); // delete if u want buttont to be clicked
       
-      message.channel.send('Hey, i am powered by https://npmjs.com/discord-buttons', button);
+      message.channel.send('Hey, i am powered by https://npmjs.com/discord-buttons', button); //sends that
 	},
 };
 
